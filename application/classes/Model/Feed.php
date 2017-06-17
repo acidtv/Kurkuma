68bdfa37f535e1b1f0e962467b1afe999bc238bb
Alex <alex@1200wd.com>
2017-06-17 20:11:56 +0200

    Disabling unread count for now, it doesnt perform well

--- application/classes/Model/Feed.php
+++ application/classes/Model/Feed.php
@@ -15,12 +15,13 @@ class Model_Feed extends ORM {
 		// my sql-fu failed me, 2 queries :(
 		$feeds = $user->feeds->find_all()->as_array();
 
-		$sql = "select uf.feed_id, count(distinct a.id) unread from users_feeds uf";
+		$sql = "select uf.feed_id, 0 as unread from users_feeds uf";
+		//$sql = "select uf.feed_id, count(distinct a.id) unread from users_feeds uf";
 		$sql .= " inner join feeds f on uf.feed_id = f.id";
-		$sql .= " left outer join articles a on f.id = a.feed_id";
-		$sql .= " left outer join users_articles_read uar on a.id = uar.article_id and uar.user_id = uf.user_id";
+		//$sql .= " left outer join articles a on f.id = a.feed_id";
+		//$sql .= " left outer join users_articles_read uar on a.id = uar.article_id and uar.user_id = uf.user_id";
 		$sql .= " where uf.user_id = :user";
-		$sql .= " and uar.user_id is null";
+		//$sql .= " and uar.user_id is null";
 		$sql .= " group by f.id";
 		$unreads = DB::query(Database::SELECT, $sql)
 			->param(':user', $user->pk())
